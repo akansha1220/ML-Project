50515d9b09086f3a97be4e45d30d4a57245a831c
Abhi Rathi <abhi@proxgy.com>
2024-09-21 12:19:01 +0530

    Add: Made issues list

--- app.py
+++ app.py
@@ -1,51 +1,59 @@
 
+from dotenv import load_dotenv
 from flask import Flask,request,render_template
-import sys
+import sys,os
 
 from sklearn.preprocessing import StandardScaler
 from src.pipline.predict_pipline import CustomData,PredictPipeline
 from src.logger import logging
 from src.exception import CustomException
 
+
+load_dotenv()
 # Register a new event source
 # Now log an event
 app=Flask(__name__)
 
 predict_pipeline=PredictPipeline()  #loading model
 
-@app.route('/',methods=['GET','POST'])
-def predict_datapoint():
+@app.route('/',methods=['GET'])
+def index():
     try:
-        if request.method=='GET':
             logging.info("Get method recieve")
             return render_template('home.html')
-        else:
-            data=CustomData(
-                gender=request.form.get('gender'),
-                race_ethnicity=request.form.get('ethnicity'),
-                parental_level_of_education=request.form.get('parental_level_of_education'),
-                lunch=request.form.get('lunch'),
-                test_preparation_course=request.form.get('test_preparation_course'),
-                reading_score=float(request.form.get('writing_score')),
-                writing_score=float(request.form.get('reading_score'))
-
-            )
-            logging.info("User inputs recieved")
-            logging.warning("User input recieved")
-
-            pred_df=data.get_data_as_dataframe()
-
-            logging.info("Mid prediction completed")
-            results=predict_pipeline.predict(pred_df)
-
-            logging.info("Get the result successfully")
-            return render_template('home.html',results=round(results[0],2))
         
     except Exception as E:
         logging.critical(f"Error occur while geting the output at {E}")
         raise CustomException(E,sys)
     
+@app.route('/predict',methods=['POST'])
+def predict_datapoint():
 
+    try:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        logging.info("User inputs recieved")
+        logging.warning("User input recieved")
+
+        pred_df=data.get_data_as_dataframe()
+
+        logging.info("Mid prediction completed")
+        results=predict_pipeline.predict(pred_df)
+
+        logging.info("Get the result successfully")
+        return render_template('home.html',results=round(results[0],2))
+    except Exception as E:
+        logging.critical(f"Error occur while geting the output at {E}")
+        raise CustomException(E,sys)
+    
 if __name__=="__main__":
-    app.run(host="0.0.0.0")  
+    app.run(host="0.0.0.0",port=int(os.getenv('APP_PORT', 3000)),debug=bool(os.getenv('DEBUG_MODE', False)))  
           
